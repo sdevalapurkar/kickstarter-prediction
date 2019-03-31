206ec9d25a79aa0e5f063f493e0bccf2387dd439
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2019-03-31 02:24:28 -0700

    code cleanup

--- split_data.py
+++ split_data.py
@@ -16,18 +16,7 @@ import seaborn as sns
 import warnings
 warnings.filterwarnings('ignore')
 from sklearn.decomposition import PCA
-
-def balancing_info(df):
-    succ = 0
-    fail = 0
-
-    for row in df:
-        if row[0] == 'failed' or row[0] == 'canceled':
-            fail += 1
-        elif row[0] == 'successful':
-            succ += 1
-
-    return succ, fail
+from sklearn.ensemble import RandomForestClassifier
 
 
 def get_list_of_main_categories(df):
@@ -50,10 +39,6 @@ def get_list_of_countries(df):
     return list_of_countries
 
 
-def convertToNumber (s):
-    return int.from_bytes(s.encode(), 'little')
-
-
 def sentiment_analyzer_scores(sentence):
     score = analyser.polarity_scores(sentence)
     return score['compound']
@@ -61,13 +46,6 @@ def sentiment_analyzer_scores(sentence):
 
 # read input file into dataset object
 dataset = pandas.read_csv('ks-projects-201801.csv')
-
-
-# new_dataset_sauce = dataset.drop('state', axis=1)
-# new_target_sauce = dataset[['state']]
-# numpy_dataset = new_dataset_sauce.to_numpy()
-# new_target_sauce = new_target_sauce.to_numpy()
-# new_target_sauce = new_target_sauce.reshape((378661,))
 
 # drop columns we don't need
 dataset = dataset.drop('ID', axis=1)
@@ -123,29 +101,12 @@ y_train_updated = y_train_updated.astype('int')
 y_test_updated = y_test.reshape((93966,))
 y_test_updated = y_test_updated.astype('int')
 
-
-
-
-
-
-
-
-# feature importance :) works!
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.datasets import load_iris
-iris = load_iris()
+# get feature importance
+# https://stackoverflow.com/questions/44101458/random-forest-feature-importance-chart-using-python
 rnd_clf = RandomForestClassifier(n_estimators=500, n_jobs=-1, random_state=42)
 rnd_clf.fit(X_train, y_train_updated)
 
-for name, importance in zip(list(dataset.drop(['state', 'name', 'deadline', 'launched'], axis=1).columns.values), rnd_clf.feature_importances_):
-    print(name, "=", importance)
-
-
-
-
-
-
-# plotting feature importances
+# plotting feature importance values
 features = list(dataset.drop(['state', 'name', 'deadline', 'launched'], axis=1).columns.values)
 importances = rnd_clf.feature_importances_
 indices = np.argsort(importances)
@@ -156,61 +117,16 @@ plt.yticks(range(len(indices)), [features[i] for i in indices])
 plt.xlabel('Relative Importance')
 plt.show()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
 # run and fit the linearsvc svm model
-# clf = LinearSVC(random_state=0, tol=1e-5)
+clf = LinearSVC(random_state=0, tol=1e-5)
 # clf = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5, 2), random_state=1)
-# clf.fit(X_train, y_train_updated)
+clf.fit(X_train, y_train_updated)
 
-# # get the model accuracy
-# score_test = clf.score(X_test, y_test_updated)
+# get the model accuracy
+score_test = clf.score(X_test, y_test_updated)
 
-# print('score test')
-# print(score_test)
-
-# from sklearn.ensemble import ExtraTreesClassifier
-# from sklearn.datasets import load_iris
-# from sklearn.feature_selection import SelectFromModel
-
-# print(X_train.shape)
-# clf = ExtraTreesClassifier(n_estimators=50)
-# clf = clf.fit(X_train, y_train)
-# print(clf.feature_importances_)
-# model = SelectFromModel(clf, prefit=True)
-# X_new = model.transform(X_train)
-# print(X_new.shape)
-
-# # feature extraction using logistic regression RFE
-# model = LogisticRegression()
-# rfe = RFE(model, 3)
-# fit = rfe.fit(X_train, y_train_updated)
-
-# print("Num Features:")
-# print(fit.n_features_)
-# print("Selected Features:")
-# print(fit.support_)
-# print("Feature Ranking:")
-# print(fit.ranking_)
-
-
-# plot.scatter(xTrain, yTrain, color = 'red')
-# plot.plot(xTrain, linearRegressor.predict(xTrain), color = 'blue')
-# plot.title('Success vs Positivity (Training set)')
-# plot.xlabel('Positivity')
-# plot.ylabel('Success')
-# plot.show()
+print('ACCURACY:')
+print(score_test)
 
 # print('Writting Datasets to CSVs:')
 # dataset_1.to_csv('test_data.csv', encoding='utf-8', index=False)
