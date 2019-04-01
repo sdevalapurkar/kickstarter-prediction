00d91803f827f0e4717941c71365ad99da418259
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2019-03-31 20:32:36 -0700

    memory error when plotting

--- split_data.py
+++ split_data.py
@@ -19,6 +19,43 @@ from sklearn.decomposition import PCA
 from sklearn.ensemble import RandomForestClassifier
 
 
+def make_meshgrid(x, y, h=.02):
+    """Create a mesh of points to plot in
+
+    Parameters
+    ----------
+    x: data to base x-axis meshgrid on
+    y: data to base y-axis meshgrid on
+    h: stepsize for meshgrid, optional
+
+    Returns
+    -------
+    xx, yy : ndarray
+    """
+    x_min, x_max = x.min() - 1, x.max() + 1
+    y_min, y_max = y.min() - 1, y.max() + 1
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+                         np.arange(y_min, y_max, h))
+    return xx, yy
+
+
+def plot_contours(ax, clf, xx, yy, **params):
+    """Plot the decision boundaries for a classifier.
+
+    Parameters
+    ----------
+    ax: matplotlib axes object
+    clf: a classifier
+    xx: meshgrid ndarray
+    yy: meshgrid ndarray
+    params: dictionary of params to pass to contourf, optional
+    """
+    Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+    out = ax.contourf(xx, yy, Z, **params)
+    return out
+
+
 def get_list_of_main_categories(df):
     list_of_categories = []
 
@@ -103,30 +140,69 @@ y_test_updated = y_test_updated.astype('int')
 
 # get feature importance
 # https://stackoverflow.com/questions/44101458/random-forest-feature-importance-chart-using-python
-rnd_clf = RandomForestClassifier(n_estimators=500, n_jobs=-1, random_state=42)
-rnd_clf.fit(X_train, y_train_updated)
+# rnd_clf = RandomForestClassifier(n_estimators=500, n_jobs=-1, random_state=42)
+# rnd_clf.fit(X_train, y_train_updated)
 
 # plotting feature importance values
-features = list(dataset.drop(['state', 'name', 'deadline', 'launched'], axis=1).columns.values)
-importances = rnd_clf.feature_importances_
-indices = np.argsort(importances)
+# features = list(dataset.drop(['state', 'name', 'deadline', 'launched'], axis=1).columns.values)
+# importances = rnd_clf.feature_importances_
+# indices = np.argsort(importances)
 
-plt.title('Feature Importances')
-plt.barh(range(len(indices)), importances[indices], color='b', align='center')
-plt.yticks(range(len(indices)), [features[i] for i in indices])
-plt.xlabel('Relative Importance')
-plt.show()
+# plt.title('Feature Importances')
+# plt.barh(range(len(indices)), importances[indices], color='b', align='center')
+# plt.yticks(range(len(indices)), [features[i] for i in indices])
+# plt.xlabel('Relative Importance')
+# plt.show()
 
 # run and fit the linearsvc svm model
-clf = LinearSVC(random_state=0, tol=1e-5)
+# clf = LinearSVC(random_state=0, tol=1e-5)
 # clf = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5, 2), random_state=1)
-clf.fit(X_train, y_train_updated)
+# clf.fit(X_train, y_train_updated)
 
 # get the model accuracy
-score_test = clf.score(X_test, y_test_updated)
+# score_test = clf.score(X_test, y_test_updated)
 
-print('ACCURACY:')
-print(score_test)
+# print('ACCURACY:')
+# print(score_test)
+
+
+
+
+# we create an instance of SVM and fit out data. We do not scale our
+# data since we want to plot the support vectors
+C = 1.0  # SVM regularization parameter
+models = (svm.SVC(kernel='linear', C=C),
+          svm.LinearSVC(C=C),
+          svm.SVC(kernel='rbf', gamma=0.7, C=C),
+          svm.SVC(kernel='poly', degree=3, C=C))
+models = (clf.fit(X_train[:, :2], y_train_updated) for clf in models)
+
+# title for the plots
+titles = ('SVC with linear kernel',
+          'LinearSVC (linear kernel)',
+          'SVC with RBF kernel',
+          'SVC with polynomial (degree 3) kernel')
+
+# Set-up 2x2 grid for plotting.
+fig, sub = plt.subplots(2, 2)
+plt.subplots_adjust(wspace=0.4, hspace=0.4)
+
+X0, X1 = X_train[:, :2][:, 0], X_train[:, :2][:, 1]
+xx, yy = make_meshgrid(X0, X1)
+
+for clf, title, ax in zip(models, titles, sub.flatten()):
+    plot_contours(ax, clf, xx, yy,
+                  cmap=plt.cm.coolwarm, alpha=0.8)
+    ax.scatter(X0, X1, c=y, cmap=plt.cm.coolwarm, s=20, edgecolors='k')
+    ax.set_xlim(xx.min(), xx.max())
+    ax.set_ylim(yy.min(), yy.max())
+    ax.set_xlabel('Sepal length')
+    ax.set_ylabel('Sepal width')
+    ax.set_xticks(())
+    ax.set_yticks(())
+    ax.set_title(title)
+
+plt.show()
 
 # print('Writting Datasets to CSVs:')
 # dataset_1.to_csv('test_data.csv', encoding='utf-8', index=False)
